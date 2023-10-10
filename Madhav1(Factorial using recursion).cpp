22dffd2b107ffee8ec336ef1c2a677dfb84a509b
Madhav Sharma <116294690+PoSeiDonXMs@users.noreply.github.com>
2023-10-10 22:57:50 +0530

    Create Madhav1(Factorial using recursion).cpp

---
+++ Madhav1(Factorial using recursion).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+	//base case
+	if(n == 1) 
+		return 1;
+	if(n == 0)
+		return 1;
+
+	//processing
+
+	//recursive relation
+	int recursionKaAns = factorial(n-1);
+
+	//processing
+	int finalAns = n * recursionKaAns;
+	return finalAns;
+}
+return 0;
+}
